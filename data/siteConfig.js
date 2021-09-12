f9baee81916f42b6da37724a6383c2d53ac17439
rbiswas-mirp <rbiswas-mirp@mit.edu>
2021-09-12 20:36:46 +0530

    tried fixes

--- data/siteConfig.js
+++ data/siteConfig.js
@@ -3,8 +3,8 @@ module.exports = {
   siteDescription: `Programmer, Web Developer, Cloud Architect`,
   keyWords: ['Sanjana Kokkonda', 'K Sanjana', 'Sanjana K'],
   authorName: 'Sanjana Kokkonda',
-  // twitterUsername: '_franciscodf',
-  // githubUsername: 'santosfrancisco',
+  twitterUsername: '_franciscodf',
+  githubUsername: 'santosfrancisco',
   authorAvatar: '/images/avatar.jpeg',
   authorDescription: `Engineering Student, passionate about what I do. Always interested in how computers worked and how the sites were made. I started to study C++ and HTML by hobby. <br />
     In 2020, I started learning C++ and HTML.
@@ -43,7 +43,7 @@ module.exports = {
         month: 'apr',
         year: '2019'
       },
-      // duration: null,
+      duration: null,
       occupation: "+2",
       description: "+2 in Junior College"
   
@@ -53,45 +53,34 @@ module.exports = {
         month: 'apr',
         year: '2017'
       },
-      // duration: '1 yr e 5 mos',
+      duration: '1 yr e 5 mos',
       occupation: "High School",
       description: "High School."
-    }, 
-    // {
-    //   company: "Anapro",
-    //   begin: {
-    //     month: 'set',
-    //     year: '2012'
-    //   },
-    //   duration: '4 yrs e 3 mos',
-    //   occupation: "Support Technician",
-    //   description: "Responsible for the implementation and parameterization of the system, training and customer support. Acting also in person in real estate launches guaranteeing the success and good use of the tool."
-  
-    // },
+    },
   ],
-  // portifolio: [
-  //   {
-  //     image: "/images/gatsby-starter-cv.png",
-  //     description: "Gatsby starter CV template",
-  //     url: "#"
-  //   },
-  //   {
-  //     image: "/images/awesome-grid.png",
-  //     description: "Responsive grid for ReactJS",
-  //     url: "#"
-  //   },
-  //   /* more portifolio items here */
-  // ],
-  // social: {
-  //   twitter: "https://twitter.com/_franciscodf",
-  //   linkedin: "https://www.linkedin.com/in/santos-francisco",
-  //   github: "https://github.com/santosfrancisco",
-  //   email: "yoshi.df@gmail.com"
-  // },
+  portifolio: [
+    {
+      image: "/images/gatsby-starter-cv.png",
+      description: "Gatsby starter CV template",
+      url: "#"
+    },
+    {
+      image: "/images/awesome-grid.png",
+      description: "Responsive grid for ReactJS",
+      url: "#"
+    },
+    /* more portifolio items here */
+  ],
+  social: {
+    twitter: "https://twitter.com/_franciscodf",
+    linkedin: "https://www.linkedin.com/in/santos-francisco",
+    github: "https://github.com/santosfrancisco",
+    email: "yoshi.df@gmail.com"
+  },
   siteUrl: 'https://www.sanjanakokkonda.in',
   pathPrefix: '/gatsby-starter-cv', // Note: it must *not* have a trailing slash.
   siteCover: '/images/cover.jpeg',
-  // googleAnalyticsId: 'UA-000000000-1',
+  googleAnalyticsId: 'UA-000000000-1',
   background_color: '#ffffff',
   theme_color: '#25303B',
   fontColor: "#000000cc",
